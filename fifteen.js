9fddddc8500f34b0a312b9d669dbe5167d208408
Jeff Zhang <jz1766american.edu>
2020-04-09 18:04:00 -0400

    Updated

--- fifteen.js
+++ fifteen.js
@@ -31,63 +31,17 @@ var movement = [
     [1, 0, 0, 1]  //15: sixteen
 ];
 
-var background_link;
-var shuffled = ids.slice();
-var moves = 0;
+var backgroundImgLink;
 
 function start() {
-    var background_id = Math.floor((Math.random() * 4));
-    background_link = background[background_id];
+  var background_id = Math.floor((Math.random() * 4));
+  backgroundImgLink = background[background_id];
 
-    document.getElementById(background[background_id]).selected = true; // Grab the selected option and mark it as selected
+  document.getElementById(background[background_id]).selected = true;
 
-    for (var i = 0; i < ids.length - 1; i++) {
-        document.getElementById(ids[i]).className = "puzzlepiece " + background[background_id];
-    }
-}
-
-function shuffle() {
-  shuffled = ids.slice();
-  var sixteen = 15;
-
-  for (var i = 0; i < 500; i++) {
-    var movement_id = Math.floor((Math.random() * 4));
-
-    while(movement[sixteen][movement_id] != 1) {
-      movement_id = Math.floor((Math.random() * 4));
-    }
-
-    var move_to;
-    switch(movement_id) {
-      case 0:
-        move_to = sixteen - 4;
-        break;
-        // subtract 4 to go to the top
-
-        case 1:
-        move_to = sixteen + 1;
-        break;
-        // add 1 to go to the right
-
-        case 2:
-        move_to = sixteen + 4;
-        break;
-        // subtract 4 to go to the bottom
-
-        case 3:
-        move_to = sixteen - 1;
-        break;
-        // subtract 1 to go to the left
-    }
-
-    // swap sixteen and move_to
-    var temp = shuffled[sixteen];
-    shuffled[sixteen] = shuffled[move_to];
-    shuffled[move_to] = temp;
-
-    sixteen = move_to;
+  for (var i = 0; i < ids.length - 1; i++) {
+    document.getElementById(ids[i]).className = "puzzlepiece " + background[background_id];
   }
-  displayBoard();
 }
 
 function displayBoard() {
@@ -99,7 +53,46 @@ function displayBoard() {
     }
     else {
       var id_name = shuffled[i];
-      document.getElementById("puzzlearea").innerHTML += '<div id="' + shuffled[i] + '" class="puzzlepiece' + " " + background_link + '">' + ids_numeric[id_name] + '</div>';
+      document.getElementById("puzzlearea").innerHTML += '<div id="' + shuffled[i] + '" class="puzzlepiece' + " " + backgroundImgLink + '">' + ids_numeric[id_name] + '</div>';
     }
   }
+}
+
+var shuffled = ids.slice();
+
+function shuffle() {
+  shuffled = ids.slice();
+  var pieces = 15;
+
+  for (var i = 0; i < 100; i++) {
+    var movement_id = Math.floor((Math.random() * 4));
+
+    while(movement[pieces][movement_id] != 1) {
+      movement_id = Math.floor((Math.random() * 4));
+    }
+
+    var move_to;
+    switch(movement_id) {
+      case 0: // subtract 4 to go to the top
+      move_to = pieces - 4;
+      break;
+      case 2: // subtract 4 to go to the bottom
+      move_to = pieces + 4;
+      break;
+      case 3: // subtract 1 to go to the left
+      move_to = pieces - 1;
+      break;
+      case 1:// add 1 to go to the right
+      move_to = pieces + 1;
+      break;
+    }
+
+    // swap sixteen and move_to
+    var temp = shuffled[pieces];
+    shuffled[pieces] = shuffled[move_to];
+    shuffled[move_to] = temp;
+
+    pieces = move_to;
+  }
+  displayBoard();
 }
